ecb763e6e322618064d4721cd7d61c75008a034e
sagi21805 <105317735+sagi21805@users.noreply.github.com>
2024-01-02 19:04:25 +0200

    Started from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,22 @@
 import 'package:flutter/material.dart';
-import 'offset.dart';
-void main() {
-  runApp(const MyApp());
+
+void main(){
+	runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-	const MyApp({super.key});
-
-	// This widget is the root of your application.
-	@override
-	Widget build(BuildContext context) {
-		return MaterialApp(
-		title: 'Flutter Demo',
-		theme: ThemeData(
-			colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 59, 224, 8)),
-			useMaterial3: true,
-		),
-		home: const MyHomePage(title: 'Flutter Demo Home Page'),
-		);
-	}
-}
-
-class MyHomePage extends StatefulWidget {
-  	const MyHomePage({super.key, required this.title});
-
-  	final String title;
-
-  	@override
-  	State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  	int _counter = 0;
-
-  	void _incrementCounter() {
-    	setState(() {
-      	_counter+=2;
-    });
-  }
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-      
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Text("Hello, World!"),
-    	), // This trailing comma makes auto-formatting nicer for build methods.
-		floatingActionButtonLocation: const CustomCenterFloatOffset(),
+	return MaterialApp(
+		home: Scaffold(
+			appBar: AppBar(
+				backgroundColor: Color.fromARGB(255, 195, 176, 145),
+				title: const Text("test title with flutter")
+				,
+			),
+		)
 	);
   }
 }
-
